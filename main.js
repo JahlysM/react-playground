330264ca5d5c9a025f463c5d4c26a9e490aecccf
Jahlys <j.manfre@laposte.net>
2022-05-17 09:47:01 +0200

    oui

--- main.js
+++ main.js
@@ -1,32 +1,35 @@
-//const helloWorld = React.createElement('h1', {}, 'Hello world!');
-
 const firstName = 'r0ulito';
 const lastName = 'formateur';
 
+function FirstName(props) {
 
-// Solution sans bonus
-const helloWorld = <h1>Hello <span>{firstName[0].toUpperCase() + firstName.slice(1)}</span> <span className="red-text">{lastName.toUpperCase()}</span></h1>;
+    /*
+    // Solution avec bonus
+    const formatFirstName = (firstName) => {
+        return firstName[0].toUpperCase() + firstName.substr(1);
+    }
+    return <span>{formatFirstName(props.text)}</span>
+    */
 
+    // Solution sans bonus
+    return <span>{props.text[0].toUpperCase() + props.text.substr(1)}</span>
+}
 
+function LastName(props) {
 
-/**
- * 
- * Solution avec bonus
- */
+    /*
+    // Solution avec bonus
+    const formatLastName = (lastName) => {
+        return lastName.toUpperCase();
+    }
+    return <span>{formatLastName(props.text)}</span>
+    */
 
-/*
- const formatFirstName = (value) => value[0].toUpperCase() + value.slice(1);
+    // Solution sans bonus
+    return <span className="red-text">{props.text.toUpperCase()}</span>
 
- const formatLastName = (value) => value.toUpperCase();
- 
- // Solution avec bonus
- const helloWorld = (
-   <h1>
-     Hello <span>{formatFirstName(firstName)}</span>{" "}
-     <span className="red-text">{formatLastName(lastName)}</span>
-   </h1>
- );
+}
 
- */
+const helloWorld = <h1>Hello <FirstName text={firstName}/> <LastName text={lastName}/></h1>;
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
